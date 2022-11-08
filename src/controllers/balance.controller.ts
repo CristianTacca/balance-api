e6203240117672ee859d373a6b7f80670fc67412
Cristian Tacca <88325995+CristianTacca@users.noreply.github.com>
2022-11-08 19:41:26 -0300

    feat: winston logger implemented.

--- src/controllers/balance.controller.ts
+++ src/controllers/balance.controller.ts
@@ -1,15 +1,18 @@
+import Logger from "../logs";
 import { Request, Response } from "express";
-import { AppError, handleError } from "../errors/appError";
 import BalanceService from "../services/balance.service";
+import { AppError, handleError } from "../errors/appError";
 
 class BalanceController {
   static get = async (req: Request, res: Response) => {
     try {
       const balance = await BalanceService.searchBalance(req);
 
+      Logger.info({ user: req.body, balance: balance });
       return res.status(201).json({ accountBalance: "R$" + balance });
     } catch (error) {
       if (error instanceof AppError) {
+        Logger.error(error);
         handleError(error, res);
       }
     }
